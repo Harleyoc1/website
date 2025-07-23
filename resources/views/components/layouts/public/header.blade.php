7a793491844803463eb3356e12e97f2ea4e20a67
Harley <Harleyoc1@gmail.com>
2025-07-23 12:53:41 +0100

    Fix routes and labels in public header

--- resources/views/components/layouts/public/header.blade.php
+++ resources/views/components/layouts/public/header.blade.php
@@ -5,17 +5,17 @@
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
-            <a href="{{ route('dashboard') }}" class="ms-2 me-5 flex items-center space-x-2 rtl:space-x-reverse lg:ms-0" wire:navigate>
+            <a href="{{ route('home') }}" class="ms-2 me-5 flex items-center space-x-2 rtl:space-x-reverse lg:ms-0" wire:navigate>
                 <x-app-logo />
             </a>
 
             <flux:spacer />
 
             <flux:navbar class="me-1.5 space-x-0.5 rtl:space-x-reverse py-0!">
-                <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="folder-git-2" href="#" :label="__('Search')">
+                <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="folder-git-2" href="#" :label="__('Portfolio')">
                     {{ __('Portfolio') }}
                 </flux:navbar.item>
-                <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="book-open-text" href="#" :label="__('Search')">
+                <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="book-open-text" href="#" :label="__('Blog')">
                     {{ __('Blog') }}
                 </flux:navbar.item>
             </flux:navbar>
