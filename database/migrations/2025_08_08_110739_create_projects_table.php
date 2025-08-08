eae40814b4a55dc1b26efe3687ece3b26f55b8b8
Harley <Harleyoc1@gmail.com>
2025-08-08 12:12:19 +0100

    Add database migration for projects

---
+++ database/migrations/2025_08_08_110739_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('tools');
+            $table->string('cover_img_path');
+            $table->text('summary');
+            $table->string('repo_link');
+            $table->boolean('standout');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
