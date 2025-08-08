2111306f662528ece32d8ae8a5d0727cd8284cd6
Harley <Harleyoc1@gmail.com>
2025-08-08 12:04:05 +0100

    Clean up example tests

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -5,14 +5,12 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class WelcomeTest extends TestCase
 {
     use RefreshDatabase;
 
     public function test_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/')->assertStatus(200);
     }
 }
