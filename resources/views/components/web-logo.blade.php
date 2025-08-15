17e563e2b6b34956f9311adbb7f46cb79ed1684b
Harley <Harleyoc1@gmail.com>
2025-08-15 14:50:25 +0100

    Replace web logo component with `flux:brand`

--- resources/views/components/web-logo.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md text-accent-foreground">
-    <img src="/images/profile-picture.jpg" class="size-8 rounded-md" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Harley O'Connor</span>
-</div>
