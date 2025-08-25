3c10b677ba006ff4f17c7dbb729c8687f0002ba2
Harley <Harleyoc1@gmail.com>
2025-08-25 13:48:19 +0100

    Fix test and add delete tests for new page
    Deleting post had not been updated for new file structure of blogs or html. Fixed this and added tests for the delete button added to the admin panel of the new page.

--- tests/Feature/Blog/ShowBlogTest.php
+++ tests/Feature/Blog/ShowBlogTest.php
@@ -6,6 +6,7 @@ use App\Livewire\Blog\ShowPost;
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Storage;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ShowBlogTest extends TestCase
@@ -57,5 +58,49 @@ class ShowBlogTest extends TestCase
 
         $this->get('/blog/test-slug')->assertSee('Some test content which we can hopefully see...');
     }
+
+    public function test_guests_cannot_delete_a_post(): void
+    {
+        Post::factory()->create(['slug' => 'test-slug']);
+
+        Livewire::test(ShowPost::class, ['slug' => 'test-slug'])
+            ->call('delete')
+            ->assertStatus(403);
+    }
+
+    public function test_non_admin_users_cannot_delete_a_post(): void
+    {
+        Post::factory()->create(['slug' => 'test-slug']);
+
+        Livewire::test(ShowPost::class, ['slug' => 'test-slug'])
+            ->call('delete')
+            ->assertStatus(403);
+    }
+
+    public function test_deleting_post_removes_the_database_row(): void
+    {
+        $this->actingAsAdmin();
+        $post = Post::factory()->create(['slug' => 'test-slug']);
+
+        Livewire::test(ShowPost::class, ['slug' => 'test-slug'])
+            ->call('delete');
+
+        $this->assertDatabaseMissing('posts', ['id' => $post->id]);
+    }
+
+    public function test_deleting_post_removes_content_files(): void
+    {
+        Storage::fake('blog');
+        $this->actingAsAdmin();
+        $post = Post::factory()->create(['slug' => 'test-slug']);
+        $post->writeContent('Some test content...');
+
+        Livewire::test(ShowPost::class, ['slug' => 'test-slug'])
+            ->call('delete');
+
+        Storage::disk('blog')->assertMissing('1/content.md');
+        Storage::disk('blog')->assertMissing('1/content.html');
+    }
+
 
 }
