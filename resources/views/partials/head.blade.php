63dda90760051357e621783e56c62b2d504eae30
Harley <Harleyoc1@gmail.com>
2025-10-10 18:34:30 +0100

    Fix failing test

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ !$title ? config('app.name') : $title.' | '.config('app.name') }}</title>
+<title>{{ isset($title) && $title ? $title.' | '.config('app.name') : config('app.name')  }}</title>
 
 <link rel="icon" href="{{ asset('/favicon.ico') }}" sizes="any">
 <link rel="apple-touch-icon" href="{{ asset('/apple-touch-icon.png') }}">
